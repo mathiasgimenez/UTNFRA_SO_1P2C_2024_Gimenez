b26035745aab757a78caf38eb21e46dc89f3cf20
Mathias <gimenezmathias2021@gmail.com>
2024-10-07 22:49:48 -0300

    segundo commit, mejoras

--- RTA_SCRIPT_Examen_20241007/Punto_B.sh
+++ RTA_SCRIPT_Examen_20241007/Punto_B.sh
@@ -16,7 +16,7 @@
         echo
 
         # crea 7 particiones logicas de 1GB
-        for i in {5..11}; do
+        for i in {5..10}; do
                 echo n
                 echo
                 echo +1G
@@ -27,26 +27,21 @@
 ) | sudo fdisk /dev/sdb
 
 # formateo de las particiones menos la particion 4 (la extendida)
-for i in {1..11}; do
+for i in {1..10}; do
         if [ $i -ne 4 ]; then
                 sudo mkfs -t ext4 /dev/sdb$i
         fi
 done
 
 # montar las partices en el directorio echo en el punto A
-cont=1
+cont=0
 for i in {1..3}; do
         for j in {1..3}; do
+		cont=$((cont + 1))
                 if [ $cont -eq 4 ]; then
                         cont=$((cont + 1))
                 fi
                 sudo mount /dev/sdb$cont /Examenes-UTN/alumno_$i/parcial_$j
                 echo "/dev/sdb$cont /Examenes-UTN/alumno_$i/parcial_$j ext4 defaults 0 0" | sudo tee -a /etc/fstab
-                cont=$((cont + 1))
         done
 done
-
-# Montar la ultima particion en el directorio de profesores
-sudo mount /dev/sdb11 /Examenes-UTN/profesores
-echo "/dev/sdb11 /Examenes-UTN/profesores ext4 defaults 0 0" | sudo tee -a /etc/fstab
-
