f16a37a8c8077b3804bee8ce42aa5992f2f1a62a
Mathias <gimenezmathias2021@gmail.com>
2024-10-07 23:58:25 -0300

    tercer commit, mejoras punto B

--- RTA_SCRIPT_Examen_20241007/Punto_B.sh
+++ RTA_SCRIPT_Examen_20241007/Punto_B.sh
@@ -15,19 +15,24 @@
         echo
         echo
 
-        # crea 7 particiones logicas de 1GB
-        for i in {5..10}; do
+        # crea 6 particiones logicas de 1GB
+        for i in {5..9}; do
                 echo n
                 echo
                 echo +1G
         done
+   
+	# la ultima particion ocupa el espacio disponible de la extendida
+	echo n
+	echo
+	echo
 
         # salir
         echo w
-) | sudo fdisk /dev/sdb
+) | sudo fdisk /dev/sdb 
 
 # formateo de las particiones menos la particion 4 (la extendida)
-for i in {1..10}; do
+for i in {1..11}; do
         if [ $i -ne 4 ]; then
                 sudo mkfs -t ext4 /dev/sdb$i
         fi
@@ -45,3 +50,5 @@ for i in {1..3}; do
                 echo "/dev/sdb$cont /Examenes-UTN/alumno_$i/parcial_$j ext4 defaults 0 0" | sudo tee -a /etc/fstab
         done
 done
+sudo mount /dev/sdb11 /Examenes-UTN/profesores
+echo "/dev/sdb11 /Examenes-UTN/profesores ext4 defaults 0 0" | sudo tee -a /etc/fstab
